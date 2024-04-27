fc5608f44324ca237c88b556312c44d2466d6710
Jake Zalesny <jrzalesny@gmail.com>
2024-04-27 15:46:37 -0600

    updated directory

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,6 @@ app.get('/', (req, res) => {
     res.send('Big Al'); // Send a response for the root URL
 });
 
-app.get('/name', (req, res) => {
-    const name = "Big Al";
-
-    res.send(name);
-})
 
 const port = process.env.port || 3000;
 
