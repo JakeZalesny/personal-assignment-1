685cc058bbc857d34e82040fd042d1fdbb25eab8
Jake Zalesny <jrzalesny@gmail.com>
2024-05-04 20:20:44 -0600

    updated index and app

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./src/routes/contacts');
+const mongodb = require('./src/connect');
+
+const app = express();
+
+app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    })
+    .use('/', router);
+
+    mongodb.initDb((err, mongodb) => {
+        if (err) {
+          console.log(err);
+        } else {
+          app.listen(port);
+          console.log(`Connected to DB and listening on ${port}`);
+        }
+      });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,6 @@
-require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const router = require('./contacts');
-const mongodb = require('../connect');
+const router = express.Router();
 
-const app = express();
+router.use('/contacts', require('./contacts'))
 
-app
-    .use(bodyParser.json())
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        next();
-    })
-    .use('/', router);
-
-    mongodb.initDb((err, mongodb) => {
-        if (err) {
-          console.log(err);
-        } else {
-          app.listen(port);
-          console.log(`Connected to DB and listening on ${port}`);
-        }
-      });
+module.exports = router;
